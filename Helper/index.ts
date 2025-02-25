a90a1f23a8607db66f94f123aa3aa9c5cb5e1abe
Ahmed idriss <webocamw224@gmail.com>
2025-02-25 16:08:55 +0000

    fix

--- Helper/index.ts
+++ Helper/index.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from '@prisma/client'
 
-const PrismaConnect = new PrismaClient()
-export default PrismaConnect
+import { PrismaClient } from '@prisma/client';
 
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const PrismaConnect = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = PrismaConnect;
+}
+
+
+
+export default PrismaConnect;
