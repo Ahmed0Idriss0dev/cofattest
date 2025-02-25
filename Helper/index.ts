af77ec2543ab0950b4e9132d5e577e435b2a2569
Ahmed idriss <webocamw224@gmail.com>
2025-02-25 16:34:10 +0000

    fix all errors

--- Helper/index.ts
+++ Helper/index.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-export const PrismaConnect = globalForPrisma.prisma ?? new PrismaClient();
+export const PrismaConnect = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = PrismaConnect;
